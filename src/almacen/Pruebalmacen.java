c70b3c2fc429496f88981c329e40b70c8531f422
Jhon <Jhon@Tugfaentanga18>
2019-03-16 00:23:54 -0500

    logica completada

--- src/almacen/Pruebalmacen.java
+++ src/almacen/Pruebalmacen.java
@@ -961,29 +961,4 @@ public class Pruebalmacen {
             hilo2.start();
         }
     }
-
-    public static void main(String[] args) {
-        Pruebalmacen almacen = new Pruebalmacen();
-        int u = 0;
-        int op = 0;
-        Scanner teclado = new Scanner(System.in);
-        do {
-            System.out.println("Escoja una opción:  ");
-            System.out.println("1:Ingresar un nuevo producto");
-            System.out.println("2:Realizar pedido");
-            System.out.println("3:terminar la op");
-            op = teclado.nextInt();
-            switch (op) {
-                case 1:
-                    almacen.ingresar_productoal();
-                    break;
-                case 2:
-                    almacen.sacar_estante();
-                    break;
-                default:
-                    System.out.println("lo sentimos esa opcion no esta en el menu");
-                    break;
-            }
-        } while (op != 3);
-    }
 }
